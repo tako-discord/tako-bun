de8fee3a0abfc358fe79a78fd633ae41b56894ec
Pukimaa <pukima@pukima.site>
2023-10-07 15:01:17 +0000

    feat(commands/sync): add sync command (devs only)

--- src/commands/sync.ts
+++ src/commands/sync.ts
@@ -1,10 +1,8 @@
-import process from 'node:process';
-import { URL } from 'node:url';
 import { API } from '@discordjs/core/http-only';
 import { REST } from 'discord.js';
 import i18next from '../i18n.ts';
 import { isDev } from '../util/checks.ts';
-import { createEmbed } from '../util/general.ts';
+import { createEmbed, getLanguage } from '../util/general.ts';
 import { loadCommands } from '../util/loaders.ts';
 import type { Command } from './index.ts';
 
@@ -14,14 +12,27 @@ export default {
 		description: 'Sync application commands with Discord',
 	},
 	async execute(interaction) {
+		const language = getLanguage(interaction.guildId);
+
 		if (!isDev(interaction.client, interaction.user.id)) {
 			const embed = createEmbed({
 				color: 'red',
-				description: i18next.t('errors:checks.devOnly.title'),
+				description: i18next.t('checks.devOnly.title', { ns: 'errors', lng: language }),
 				emoji: 'error',
-				title: i18next.t('errors:checks.devOnly.title'),
+				title: i18next.t('checks.devOnly.title', { ns: 'errors', lng: language }),
 			});
 			await interaction.reply({ embeds: [embed], ephemeral: true });
+			return;
 		}
+
+		const commands = await loadCommands(Bun.pathToFileURL(import.meta.dir));
+		const commandData = [...commands.values()].map((command) => command.data);
+
+		const rest = new REST({ version: '10' }).setToken(Bun.env.DISCORD_TOKEN!);
+		const api = new API(rest);
+
+		const result = await api.applicationCommands.bulkOverwriteGlobalCommands(Bun.env.APPLICATION_ID!, commandData);
+
+		await interaction.reply({ content: `Successfully registered ${result.length} commands.` });
 	},
 } satisfies Command;
