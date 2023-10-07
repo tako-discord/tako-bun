ecb875227a79ceaa44a62e91809d7a14286fe208
Pukimaa <pukima@pukima.site>
2023-10-07 15:00:45 +0000

    fix(commands/ping): proper i18n for ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,5 +1,5 @@
 import i18next from '../i18n.ts';
-import { createEmbed } from '../util/general.ts';
+import { createEmbed, getLanguage } from '../util/general.ts';
 import type { Command } from './index.ts';
 
 export default {
@@ -9,12 +9,13 @@ export default {
 	},
 	async execute(interaction) {
 		const ping = interaction.client.ws.ping;
+		const language = await getLanguage(interaction.guildId);
 
 		const embed = createEmbed({
 			color: ping < 200 ? 'green' : ping < 400 ? 'yellow' : 'red',
-			title: i18next.t('info:ping.title'),
+			title: i18next.t('ping.title', { ns: 'info', lng: language }),
 			emoji: 'ping',
-			description: i18next.t('info:ping.response', { latency: ping }),
+			description: i18next.t('ping.response', { ns: 'info', lng: language, latency: ping }),
 		});
 		await interaction.reply({ embeds: [embed] });
 	},
