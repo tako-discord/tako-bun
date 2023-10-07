2ba3731ed83ef75d8ee2ec679446f79834351a80
Pukimaa <pukima@pukima.site>
2023-10-07 14:59:31 +0000

    feat(commands/info): add `/info user` command

---
+++ src/@types/general.d.ts
@@ -0,0 +1,11 @@
+import type config from '../../config.ts';
+
+export type EmbedOptions = {
+	color: number | keyof typeof config.colors;
+	description?: string;
+	emoji?: keyof typeof config.emojis;
+	fields?: APIEmbedField[];
+	image?: string | null;
+	thumbnail?: string;
+	title: string;
+};
--- src/util/general.ts
+++ src/util/general.ts
@@ -1,22 +1,56 @@
-import type { APIEmbedField } from 'discord.js';
+import type { Client } from 'discord.js';
 import { EmbedBuilder } from 'discord.js';
 import config from '../../config.ts';
+import type { EmbedOptions } from '../@types/general';
+import prisma from '../database.ts';
 
-type EmbedOptions = {
-	color: keyof typeof config.colors;
-	description?: string;
-	emoji?: keyof typeof config.emojis;
-	fields?: APIEmbedField[];
-	title: string;
-};
-
-export function createEmbed({ color = 'primary', description, emoji, fields, title }: EmbedOptions) {
+export function createEmbed({ color = 'primary', description, emoji, fields, image, thumbnail, title }: EmbedOptions) {
 	const embed = new EmbedBuilder()
-		.setColor(config.colors[color])
+		.setColor(typeof color === 'string' ? config.colors[color] : color)
+		// eslint-disable-next-line @typescript-eslint/no-base-to-string
 		.setTitle(`${emoji ? config.emojis[emoji] : ''} ${title}`);
 
 	if (description) embed.setDescription(description);
 	if (fields) embed.setFields(fields);
+	if (thumbnail) embed.setThumbnail(thumbnail);
+	if (image) embed.setImage(image);
 
 	return embed;
+}
+
+export async function getColor(guildId: string | null, userId?: string, client?: Client) {
+	let color = config.colors.primary;
+	if (guildId) {
+		const guild = await prisma.guild.findFirst({ where: { id: guildId } });
+		if (guild?.color) color = guild.color;
+	}
+
+	if (userId) {
+		const user = await prisma.user.findFirst({ where: { id: userId } });
+
+		if (!user?.color && client) {
+			const user = await client.users.fetch(userId, { force: true });
+			if (user) color = user.accentColor ?? color;
+			return color;
+		}
+
+		color = user?.color ?? color;
+	}
+
+	return color;
+}
+
+export async function getLanguage(guildId: string | null) {
+	let language = 'en';
+	if (guildId) {
+		const guild = await prisma.guild.findFirst({ where: { id: guildId } });
+		if (guild?.language) language = guild.language;
+	}
+
+	return language;
+}
+
+export async function getBanner(userId: string) {
+	const user = await prisma.user.findFirst({ where: { id: userId } });
+	return user?.background;
 }
