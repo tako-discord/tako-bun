c13c51a29854e7d68700efc3d551f2e805a82796
Pukimaa <pukima@pukima.site>
2023-10-14 17:28:29 +0000

    feat(commands/info): localize command data

--- src/commands/info/info.ts
+++ src/commands/info/info.ts
@@ -1,17 +1,28 @@
 import { SlashCommandBuilder } from 'discord.js';
+import i18next from '../../i18n.ts';
+import { slashCommandTranslator } from '../../util/general.ts';
 import { userInfo } from '../../util/info.ts';
 import type { Command } from '../index.ts';
 
 export default {
 	data: new SlashCommandBuilder()
-		.setName('info')
-		.setDescription('Get info about anything')
+		.setName(i18next.t('info.name', { ns: 'info' }))
+		.setNameLocalizations(slashCommandTranslator('info.name', 'info'))
+		.setDescription(i18next.t('info.description', { ns: 'info' }))
+		.setDescriptionLocalizations(slashCommandTranslator('info.description', 'info'))
 		.addSubcommand((subcommand) =>
 			subcommand
-				.setName('user')
-				.setDescription('Get info about a user or yourself')
+				.setName(i18next.t('info.user.name', { ns: 'info' }))
+				.setNameLocalizations(slashCommandTranslator('info.user.name', 'info'))
+				.setDescription(i18next.t('info.user.description', { ns: 'info' }))
+				.setDescriptionLocalizations(slashCommandTranslator('info.user.description', 'info'))
 				.addUserOption((option) =>
-					option.setName('target').setDescription('The user to get the info from').setRequired(false),
+					option
+						.setName(i18next.t('info.user.options.user.name', { ns: 'info' }))
+						.setNameLocalizations(slashCommandTranslator('info.user.options.user.name', 'info'))
+						.setDescription(i18next.t('info.user.options.user.description', { ns: 'info' }))
+						.setDescriptionLocalizations(slashCommandTranslator('info.user.options.user.description', 'info'))
+						.setRequired(false),
 				),
 		)
 		.toJSON(),
