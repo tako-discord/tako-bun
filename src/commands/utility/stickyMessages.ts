11f4e1ef8b56be08e4e21cf76567e3ff81085f3b
Pukimaa <pukima@pukima.site>
2023-10-15 13:29:27 +0000

    feat(commands/sticky-message): localize command data

--- src/commands/utility/stickyMessages.ts
+++ src/commands/utility/stickyMessages.ts
@@ -1,22 +1,38 @@
 import type { ChatInputCommandInteraction, ModalActionRowComponentBuilder } from 'discord.js';
 import { ActionRowBuilder, ModalBuilder, SlashCommandBuilder, TextInputBuilder, TextInputStyle } from 'discord.js';
 import i18next from '../../i18n.ts';
-import { getLanguage } from '../../util/general.ts';
+import { getLanguage, slashCommandTranslator } from '../../util/general.ts';
 import type { Command } from '../index.ts';
 
 export default {
 	data: new SlashCommandBuilder()
-		.setName('sticky-message')
-		.setDescription('Set a sticky message for a channel. That message will always be at the bottom of the channel.')
+		.setName(i18next.t('stickyMessages.name', { ns: 'utility' }))
+		.setNameLocalizations(slashCommandTranslator('stickyMessages.name', 'utility'))
+		.setDescription(i18next.t('stickyMessages.description', { ns: 'utility' }))
+		.setDescriptionLocalizations(slashCommandTranslator('stickyMessages.description', 'utility'))
 		.addSubcommand((subcommand) =>
 			subcommand
-				.setName('set')
-				.setDescription('Set the sticky message')
+				.setName(i18next.t('stickyMessages.set.name', { ns: 'utility' }))
+				.setNameLocalizations(slashCommandTranslator('stickyMessages.set.name', 'utility'))
+				.setDescription(i18next.t('stickyMessages.set.description', { ns: 'utility' }))
+				.setDescriptionLocalizations(slashCommandTranslator('stickyMessages.set.description', 'utility'))
 				.addChannelOption((option) =>
-					option.setName('channel').setDescription('The channel to create the sticky message in'),
+					option
+						.setName(i18next.t('stickyMessages.set.options.channel.name', { ns: 'utility' }))
+						.setNameLocalizations(slashCommandTranslator('stickyMessages.set.options.channel.name', 'utility'))
+						.setDescription(i18next.t('stickyMessages.set.options.channel.description', { ns: 'utility' }))
+						.setDescriptionLocalizations(
+							slashCommandTranslator('stickyMessages.set.options.channel.description', 'utility'),
+						),
 				)
 				.addBooleanOption((option) =>
-					option.setName('embed').setDescription('Whether or not to send the message as an embed'),
+					option
+						.setName(i18next.t('stickyMessages.set.options.embed.name', { ns: 'utility' }))
+						.setNameLocalizations(slashCommandTranslator('stickyMessages.set.options.embed.name', 'utility'))
+						.setDescription(i18next.t('stickyMessages.set.options.embed.description', { ns: 'utility' }))
+						.setDescriptionLocalizations(
+							slashCommandTranslator('stickyMessages.set.options.embed.description', 'utility'),
+						),
 				),
 		)
 		.toJSON(),
@@ -29,14 +45,14 @@ export default {
 
 			const modal = new ModalBuilder()
 				.setCustomId('stickyMessage')
-				.setTitle(i18next.t('stickyMessages.modal.title', { ns: 'utility', lng: language }));
+				.setTitle(i18next.t('stickyMessages.set.modal.title', { ns: 'utility', lng: language }));
 
 			const message = new TextInputBuilder()
 				.setCustomId('stickyMessage-message')
-				.setLabel(i18next.t('stickyMessages.modal.label', { ns: 'utility', lng: language }))
+				.setLabel(i18next.t('stickyMessages.set.modal.label', { ns: 'utility', lng: language }))
 				.setStyle(TextInputStyle.Paragraph)
 				.setMaxLength(maxLength)
-				.setPlaceholder(i18next.t('stickyMessages.modal.placeholder', { ns: 'utility', lng: language }))
+				.setPlaceholder(i18next.t('stickyMessages.set.modal.placeholder', { ns: 'utility', lng: language }))
 				.setMinLength(1)
 				.setRequired(true);
 
